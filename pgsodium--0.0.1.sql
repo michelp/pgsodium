7da28a0373c929b1e863a42acbb1528b15d0ebc3
Michel Pelletier <michelp@users.noreply.github.com>
2017-04-16 17:19:20 -0700

    added randombytes_buf and uniform

--- pgsodium--0.0.1.sql
+++ pgsodium--0.0.1.sql
@@ -1,3 +1,19 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgsodium" to load this file. \quit
 
+CREATE FUNCTION pgsodium_randombytes_random()
+RETURNS integer
+AS '$libdir/pgsodium'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION pgsodium_randombytes_uniform(integer)
+RETURNS integer
+AS '$libdir/pgsodium'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION pgsodium_randombytes_buf(integer)
+RETURNS bytea
+AS '$libdir/pgsodium'
+LANGUAGE C IMMUTABLE STRICT;
+
+
