258ef85debbf1fbb3b2f77eafecf1bdc3610f1d3
Michel Pelletier <pelletier.michel@gmail.com>
2020-06-05 13:52:07 -0700

    add psql script to fire up test dbs.

---
+++ psql.sh
@@ -0,0 +1,25 @@
+version=$1
+
+DB_HOST="pgsodium-test-db-$version"
+DB_NAME="postgres"
+SU="postgres"
+EXEC="docker exec $DB_HOST"
+TAG="pgsodium/test-$version"
+
+echo building test image $DB_HOST
+docker build . -t $TAG --build-arg "version=$version"
+
+echo running test container
+docker run -e POSTGRES_HOST_AUTH_METHOD=trust -d --name "$DB_HOST" $TAG
+
+echo waiting for database to accept connections
+until
+    $EXEC \
+        psql -o /dev/null -t -q -U "$SU" \
+        -c 'select pg_sleep(1)' \
+        2>/dev/null;
+do sleep 1;
+done
+
+echo running tests
+docker exec -it $DB_HOST psql -U "$SU"
