7d01cbe96f0945fe303b4091bd90ddf2430cd25c
Michel Pelletier <pelletier.michel@gmail.com>
2022-08-26 17:49:44 -0700

    some more key tests.

--- test/keys.sql
+++ test/keys.sql
@@ -1,8 +1,8 @@
 \if :serverkeys
 BEGIN;
-SELECT plan(3);
+SELECT plan(4);
 
-select * from pgsodium.create_key('aead-det') \gset anon_det_key_
+select * from pgsodium.create_key() \gset anon_det_key_
 
 select results_eq(
   format($$select
@@ -11,9 +11,10 @@ select results_eq(
   raw_key is null,
   parent_key is null,
   raw_key_nonce is null,
-  name is null
+  name is null,
+  key_type = 'aead-det'
   from pgsodium.key where id = %L$$, :'anon_det_key_id'),
-  'values (true, true, true, true, true, true)',
+  'values (true, true, true, true, true, true, true)',
   'anon det key asserts');
 
 select * from pgsodium.create_key('aead-det', 'foo') \gset foo_det_key_
@@ -25,9 +26,10 @@ select results_eq(
   raw_key is null,
   parent_key is null,
   raw_key_nonce is null,
+  key_type = 'aead-det',
   name = 'foo'
   from pgsodium.key where id = %L$$, :'foo_det_key_id'),
-  'values (true, true, true, true, true, true)',
+  'values (true, true, true, true, true, true, true)',
   'named det key asserts');
 
 
@@ -41,9 +43,26 @@ select results_eq(
   raw_key is not null,
   parent_key is not null,
   raw_key_nonce is not null,
-  name = 'stripe'
+  name = 'stripe',
+  key_type = 'aead-det'
   from pgsodium.key where id = %L$$,:'stripe_hmac256_key_id'),
-  'values (true, true, true, true, true, true)',
+  'values (true, true, true, true, true, true, true)',
+  'ext key asserts');
+
+select * from pgsodium.create_key(
+     'hmacsha256', 'stripe2', parent_key:=:'anon_det_key_id', raw_key:=:'ext_hmac256_key') \gset stripe_hmac256_key_
+
+select results_eq(
+  format($$select
+  key_id is null,
+  key_context is null,
+  raw_key is not null,
+  parent_key = %L,
+  raw_key_nonce is not null,
+  name = 'stripe',
+  key_type = 'aead-det'
+  from pgsodium.key where id = %L$$, :'anon_det_key_id', :'stripe_hmac256_key_id'),
+  'values (true, true, true, true, true, true, true)',
   'ext key asserts');
 
 SELECT * FROM finish();
