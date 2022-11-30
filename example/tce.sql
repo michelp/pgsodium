7318acc3e0b705b5676815b05ca93b7252bf9fd4
Michel Pelletier <michelp@users.noreply.github.com>
2022-11-29 22:20:29 -0800

    tests for quoted table names. (#51)

--- example/tce.sql
+++ example/tce.sql
@@ -4,10 +4,10 @@
 
 CREATE EXTENSION IF NOT EXISTS pgsodium;
 
-DROP SCHEMA IF EXISTS tce_example CASCADE;
-CREATE SCHEMA tce_example;
+DROP SCHEMA IF EXISTS "tce-example" CASCADE;
+CREATE SCHEMA "tce-example";
 
-SET search_path = tce_example, pg_catalog;
+SET search_path = "tce-example", pg_catalog;
 
 CREATE TABLE test (
   secret text
@@ -25,10 +25,10 @@ CREATE TABLE test2 (
 );
 
 CREATE ROLE bob with login password 'foo';
-GRANT INSERT ON tce_example.test, tce_example.test2 to bob;
+GRANT INSERT ON "tce-example".test, "tce-example".test2 to bob;
 GRANT USAGE ON SEQUENCE test2_id_seq to bob;
 
-SECURITY LABEL FOR pgsodium ON ROLE bob is 'ACCESS tce_example.test, tce_example.test2';
+SECURITY LABEL FOR pgsodium ON ROLE bob is 'ACCESS "tce-example".test, "tce-example".test2';
 
 SELECT format('ENCRYPT WITH KEY ID %s', (pgsodium.create_key('aead-det')).id)
     AS seclabel \gset
@@ -40,34 +40,39 @@ SELECT id AS secret2_key_id FROM pgsodium.create_key('aead-det', 'foo_key') \gse
 
 SECURITY LABEL FOR pgsodium	ON COLUMN test.secret IS :'seclabel';
 
-SECURITY LABEL FOR pgsodium ON TABLE tce_example.test2 IS
-    'DECRYPT WITH VIEW tce_example.other_test2';
+SECURITY LABEL FOR pgsodium ON TABLE "tce-example".test2 IS
+    'DECRYPT WITH VIEW "tce-example"."other-test2"';
 
 SECURITY LABEL FOR pgsodium	ON COLUMN test2.secret IS :'seclabel2';
 
-SECURITY LABEL FOR pgsodium	ON COLUMN tce_example.test2.secret2 IS
+SECURITY LABEL FOR pgsodium	ON COLUMN "tce-example".test2.secret2 IS
     'ENCRYPT WITH KEY COLUMN secret2_key_id ASSOCIATED (id, associated2) NONCE nonce2';
 
 SELECT pgsodium.crypto_aead_det_noncegen() aead_nonce \gset
 SELECT pgsodium.crypto_aead_det_noncegen() aead_nonce2 \gset
 
-GRANT ALL ON SCHEMA tce_example TO bob;
+GRANT ALL ON SCHEMA "tce-example" TO bob;
+select pgsodium.update_masks(true);
+
 COMMIT;
 \c postgres bob
 \x
 
-SET search_path = tce_example, pg_catalog;
+SET search_path = "tce-example", pg_catalog;
 
-INSERT INTO tce_example.decrypted_test (secret) VALUES ('noice') RETURNING *;
+INSERT INTO "tce-example".decrypted_test (secret) VALUES ('noice') RETURNING *;
 
-INSERT INTO tce_example.other_test2 (secret, associated, nonce, secret2, associated2, nonce2, secret2_key_id)
+INSERT INTO "tce-example"."other-test2" (secret, associated, nonce, secret2, associated2, nonce2, secret2_key_id)
     VALUES ('sssh', 'bob was here', :'aead_nonce', 'aaahh', 'alice association', :'aead_nonce2', :'secret2_key_id'::uuid) RETURNING *;
 
-CREATE TABLE tce_example.bob_test (
-  secret text
+CREATE TABLE "tce-example"."bob-testt" (
+  "secret2-test" text,
+  "associated2-test" text,
+  "secret2_key_id-test" uuid DEFAULT (pgsodium.create_key()).id,
+  "nonce2-test" bytea DEFAULT pgsodium.crypto_aead_det_noncegen()
 );
 
-SELECT format('ENCRYPT WITH KEY ID %s', (pgsodium.create_key('aead-det', 'bob_key')).id)
-    AS seclabel \gset
+SECURITY LABEL FOR pgsodium	ON COLUMN "bob-testt"."secret2-test" IS
+    'ENCRYPT WITH KEY COLUMN secret2_key_id-test ASSOCIATED (associated2-test) NONCE nonce2-test';
 
-SECURITY LABEL FOR pgsodium	ON COLUMN bob_test.secret IS :'seclabel';
+select pgsodium.update_masks(true);
--- test/tce.sql
+++ test/tce.sql
@@ -1,8 +1,9 @@
 \if :serverkeys
 BEGIN;
-SELECT plan(6);
+SELECT plan(10);
 
 CREATE SCHEMA private;
+CREATE SCHEMA "private-test";
 
 SELECT throws_ok(
   $test$
@@ -12,19 +13,39 @@ SELECT throws_ok(
   'pgsodium provider does not support labels on this object',
   'schemas cannot be labled');
 
+SELECT throws_ok(
+  $test$
+  SECURITY LABEL FOR pgsodium ON SCHEMA "private-test" IS 'nope'
+  $test$,
+  '0A000',
+  'pgsodium provider does not support labels on this object',
+  'quoted schema cannot be labled');
+
 CREATE TABLE private.foo(
   secret text,
   associated text default ''
 );
 
+CREATE TABLE "private-test"."foo-test"(
+  "secret-test" text,
+  "associated-test" text default ''
+);
+
 CREATE TABLE private.bar(
   id bigserial primary key,
-  secret text DEFAULT '',
+  secret text,
   nonce bytea  DEFAULT pgsodium.crypto_aead_det_noncegen(),
-  secret2 text DEFAULT '',
-  associated2 text DEFAULT '',
-  secret2_key_id uuid,
+  secret2 text,
+  associated2 text,
+  secret2_key_id uuid DEFAULT (pgsodium.create_key()).id,
   nonce2 bytea DEFAULT pgsodium.crypto_aead_det_noncegen()
+);
+
+CREATE TABLE "private-test"."bar-test"(
+  "secret2-test" text,
+  "associated2-test" text,
+  "secret2_key_id-test" uuid DEFAULT (pgsodium.create_key()).id,
+  "nonce2-test" bytea DEFAULT pgsodium.crypto_aead_det_noncegen()
 );
 
 SELECT lives_ok(
@@ -32,6 +53,12 @@ SELECT lives_ok(
   SECURITY LABEL FOR pgsodium ON TABLE private.bar IS 'DECRYPT WITH VIEW private.other_bar'
   $test$,
   'tables can be labeled with alternate view');
+
+SELECT lives_ok(
+  $test$
+  SECURITY LABEL FOR pgsodium ON TABLE "private-test"."bar-test" IS 'DECRYPT WITH VIEW "private-test"."other_bar-test"'
+  $test$,
+  'quoted tables can be labeled with quoted alternate view');
 
 -- Create a key id to use in the tests below
 SELECT id AS secret_key_id FROM pgsodium.create_key('aead-det', 'OPTIONAL_NAME') \gset
@@ -54,16 +81,18 @@ SELECT lives_ok(
          $test$, :'secret_key_id'),
   'can label another column on same table for encryption');
 
-CREATE ROLE bobo with login password 'foo';
+SELECT lives_ok(
+  format($test$
+         SECURITY LABEL FOR pgsodium ON COLUMN "private-test"."foo-test"."secret-test"
+         IS 'ENCRYPT WITH KEY ID %s'
+         $test$, :'secret_key_id'),
+  'can label quoted column for encryption');
 
-GRANT SELECT ON pgsodium.key TO pgsodium_keyholder;
-GRANT ALL ON SCHEMA private to bobo;
-GRANT SELECT ON ALL TABLES IN SCHEMA private to bobo;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA private TO bobo;
+CREATE ROLE bobo with login password 'foo';
 
 SELECT lives_ok(
   $test$
-  SECURITY LABEL FOR pgsodium ON ROLE bobo is 'ACCESS private.foo, private.bar'
+  SECURITY LABEL FOR pgsodium ON ROLE bobo is 'ACCESS private.foo, private.bar, private-test.other_bar-test'
   $test$,
   'can label roles ACCESS');
 
@@ -74,13 +103,29 @@ SELECT lives_ok(
   $test$),
   'can label another column for encryption');
 
+SELECT lives_ok(
+  format($test$
+         SECURITY LABEL FOR pgsodium ON COLUMN "private-test"."bar-test"."secret2-test"
+         IS 'ENCRYPT WITH KEY COLUMN secret2_key_id-test ASSOCIATED (associated2-test) NONCE nonce2-test'
+  $test$),
+  'can label another quoted column for encryption');
+
+GRANT SELECT ON pgsodium.key TO pgsodium_keyholder;
+GRANT ALL ON SCHEMA private to bobo;
+GRANT SELECT ON ALL TABLES IN SCHEMA private to bobo;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA private TO bobo;
+
+GRANT ALL ON SCHEMA "private-test" to bobo;
+GRANT ALL ON ALL TABLES IN SCHEMA "private-test" to bobo;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA "private-test" TO bobo;
+
 SELECT * FROM finish();
 COMMIT;
 
 \c - bobo
 
 BEGIN;
-SELECT plan(12);
+SELECT plan(15);
 
 SELECT pgsodium.crypto_aead_det_noncegen() nonce \gset
 SELECT pgsodium.crypto_aead_det_noncegen() nonce2 \gset
@@ -104,20 +149,28 @@ SELECT results_eq($$SELECT decrypted_secret = 'sp00n' from private.decrypted_foo
     'can see updated decrypted view');
 
 SELECT lives_ok(
-  format(
     $test$
-    INSERT INTO private.other_bar (secret, nonce, secret2, associated2, nonce2, secret2_key_id)
-    VALUES ('s3kr3t', %L, 'shhh', 'bob was here', %L, %L::uuid);
+    INSERT INTO private.other_bar (secret2, associated2)
+    VALUES ('s3kr3t', 'bob was here');
     $test$,
-    :'nonce',
-    :'nonce2',
-    :'secret2_key_id'),
-    'can insert into bar table');
+    'can insert into other bar');
+
+SELECT lives_ok(
+    $test$
+    INSERT INTO "private-test"."bar-test" ("secret2-test", "associated2-test")
+    VALUES ('s3kr3t-test', 'bob was here');
+    $test$,
+    'can insert into quoted private bar');
 
 SELECT results_eq(
-    $$SELECT decrypted_secret = 's3kr3t', decrypted_secret2 = 'shhh' FROM private.other_bar$$,
-    $$VALUES (true,  true)$$,
+    $$SELECT decrypted_secret2 = 's3kr3t' FROM private.other_bar$$,
+    $$VALUES (true)$$,
     'can select from masking view');
+
+SELECT results_eq(
+    $$SELECT "decrypted_secret2-test" = 's3kr3t-test' FROM "private-test"."other_bar-test"$$,
+    $$VALUES (true)$$,
+    'can select from quoted masking view');
 
 SELECT id AS another_secret_key_id FROM pgsodium.create_key(name:='ANOTHER_NAME') \gset
 
@@ -128,9 +181,21 @@ SELECT lives_ok(
     $test$, :'another_secret_key_id'),
     'can update key id with rotation into decrypted view');
 
-SELECT results_eq($$SELECT decrypted_secret2 = 'shhh' from private.other_bar$$,
+SELECT results_eq($$SELECT decrypted_secret2 = 's3kr3t' from private.other_bar$$,
     $$VALUES (true)$$,
     'can see updated key id in decrypted view');
+
+SELECT lives_ok(
+  format(
+    $test$
+    INSERT INTO private.other_bar (secret, nonce, secret2, associated2, nonce2, secret2_key_id)
+    VALUES ('s3kr3t', %L, 'shhh', 'bob was here', %L, %L::uuid);
+    $test$,
+    :'nonce',
+    :'nonce2',
+    :'secret2_key_id'),
+    'can insert into bar table');
+
 
 CREATE TABLE private.bobo(
   secret text,
