7be8854c28ad8f99d4162128773cdfbbab6a788a
Andrew Wasielewski <mail@andrewwasielewski.com>
2023-02-07 16:22:24 -0500

    remove nonfunctional powershell root key script

--- getkey_scripts/pgsodium_getkey_windows.ps1
+++
@@ -1,7 +0,0 @@
-$KEY_FILE = "$PGDATA/pgsodium_root.key"
-
-if (-not(Test-Path -Path $KEY_FILE -PathType Leaf))
-{
-  (1..64|foreach-object {'{0:x}' -f (Get-Random -Maximum 16)}) -join '' > $KEY_FILE
-}
-cat $KEY_FILE
