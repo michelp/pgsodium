b5da330c7003e2a2bc44178697334b0a6ed45630
Jehan-Guillaume de Rorthais <jgdr@dalibo.com>
2023-06-10 01:09:32 +0200

    Avoid rebuilding the docker image when possible

--- test.sh
+++ test.sh
@@ -6,16 +6,17 @@ versions=${1:-13 14 15}
 
 for version in $versions
 do
+	TAG="pgsodium/test-$version"
+	DB_HOST="pgsodium-test-db-$version"
+
+	echo building test image $DB_HOST
+	docker build . -t $TAG --build-arg "version=$version"
+
 	for config in '-c shared_preload_libraries=pgsodium' '-c shared_preload_libraries=pgsodium -c pgsodium.getkey_script=/getkey' ''
 	do
-		DB_HOST="pgsodium-test-db-$version"
 		DB_NAME="postgres"
 		SU="postgres"
 		EXEC="docker exec -i $DB_HOST"
-		TAG="pgsodium/test-$version"
-
-		echo building test image $DB_HOST
-		docker build . -t $TAG --build-arg "version=$version"
 
 		echo running test container
 		docker run --rm -e POSTGRES_HOST_AUTH_METHOD=trust -d \
